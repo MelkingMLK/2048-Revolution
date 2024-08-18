3b93338b350be9a772b460e0bc232cf0fe5856d6
MelkingMMV <alessandro.melnykam@gmail.com>
2024-08-18 21:49:35 +0200

    Part II-  Login,register

--- app.py
+++ app.py
@@ -5,13 +5,20 @@ button_color = (137, 37, 238)
 hover_color = (100, 160, 210)
 text_color = (255, 255, 255)
 title_color = (255, 85, 208)
+log_color = (246, 248, 255)
+
 copyright_color = (128, 128, 128)
+STATE_MAIN_MENU = "main_menu"
+STATE_REGISTER = "register"
+STATE_Login = "login"
+STATE_MODALITA_1 = "modalita_1"
+STATE_MODALITA_2 = "modalita_2"
+current_state = STATE_MAIN_MENU  
 #######################################################################################
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((1680, 1050))
 pygame.display.set_caption("2048 Revolution")
-background_image = pygame.image.load("./sf1.gif").convert()
 font = pygame.font.Font(None, 40)
 title_font = pygame.font.Font("./Font/DnaBeast-MDEv.ttf", 40)
 copyright_font = pygame.font.Font("./Font/PrStart.ttf", 10)
@@ -23,30 +30,59 @@ def Button(rect, text, is_hovered):
     text_surface = font.render(text, True, text_color)
     text_rect = text_surface.get_rect(center=rect.center)
     screen.blit(text_surface, text_rect)
-def Register()
+
+def Main():
+    background_image = pygame.image.load("./sf1.gif").convert()
+    screen.blit(background_image, (0, 0))
+
+    title_text = title_font.render("2048 Revolution", True, title_color)
+    title_rect = title_text.get_rect()
+    title_rect.topleft = (150, 150)
+    screen.blit(title_text, title_rect)
+
+    copyright_text = copyright_font.render("@Mlk Company 2024", True, copyright_color)
+    copyright_rect = copyright_text.get_rect()
+    copyright_rect.topleft = (0, 1030)
+    screen.blit(copyright_text, copyright_rect)
     
-def modalita_1():
-    global background_image
+    mouse_pos = pygame.mouse.get_pos()
+    Button(button1, "Login", button1.collidepoint(mouse_pos))
+    Button(button2, "Register", button2.collidepoint(mouse_pos))
+
+def Register():
+    StopMusic()
+    background_image = pygame.image.load("./sf4.jpg").convert()
+    screen.blit(background_image, (0, 0))
+    title_text = title_font.render("Register ", True, log_color)
+    title_rect = title_text.get_rect()
+    title_rect.topleft = (650, 50)
+    screen.blit(title_text, title_rect)
+
+def Login():
+    StopMusic()
+    background_image = pygame.image.load("./sf4.jpg").convert()
+    screen.blit(background_image, (0, 0))
+    title_text = title_font.render("Login ", True, log_color)
+    title_rect = title_text.get_rect()
+    title_rect.topleft = (650, 50)
+    screen.blit(title_text, title_rect)
+
+#def Mod1():
+
+
+
+
+def play_music(file, volume=0.15):
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play(-1)
+    pygame.mixer.music.set_volume(volume)
+
+def StopMusic():
     pygame.mixer.music.stop()
-    background_image = pygame.image.load("./sf2.jpg").convert()
-
-def modalita_2():
-    print("Modalità 2 selezionata")
-#######################################################################################
-pygame.mixer.music.load("./jojo.mp3")
-pygame.mixer.music.play(-1)
-pygame.mixer.music.set_volume(0.15)
-
-title_text = title_font.render("2048 Revolution", True, title_color)
-title_rect = title_text.get_rect()
-title_rect.topleft = (150, 150)
-copyright_text = copyright_font.render("@Mlk Company 2024", True, copyright_color)
-copyright_rect = copyright_text.get_rect()
-copyright_rect.topleft = (0, 1030)  
 
 button1 = pygame.Rect(350, 350, 300, 60)
 button2 = pygame.Rect(350, 450, 300, 60)
-
+play_music("./jojo.mp3")
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -54,15 +90,18 @@ while True:
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
-            if button1.collidepoint(mouse_pos):
-                modalita_1()
-            elif button2.collidepoint(mouse_pos):
-                modalita_2()
-    
-    screen.blit(background_image, (0, 0))
-    screen.blit(title_text, title_rect)
-    screen.blit(copyright_text, copyright_rect)  # Draw the copyright text
-    mouse_pos = pygame.mouse.get_pos()
-    Button(button1, "Login", button1.collidepoint(mouse_pos))
-    Button(button2, "Register", button2.collidepoint(mouse_pos))
+            if current_state == STATE_MAIN_MENU:
+                if button1.collidepoint(mouse_pos):
+                    current_state = STATE_REGISTER
+                elif button2.collidepoint(mouse_pos):
+                    current_state = STATE_REGISTER
+    if current_state == STATE_MAIN_MENU:
+        Main()
+    elif current_state == STATE_REGISTER:
+        Register()
+    elif current_state == STATE_MODALITA_1:
+          Main() #Mod1()
+    elif current_state == STATE_MODALITA_2:
+          Main() #Mod2()
+
     pygame.display.flip()
